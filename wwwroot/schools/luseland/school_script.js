d8b180594619da94a835fd24dd5768f8d959512c
Mike S <mike.strendin@lskysd.ca>
2020-05-27 23:43:32 -0600

    Adds Luseland school as I missed adding this school's main files when I first set up all the schools.

---
+++ wwwroot/schools/luseland/school_script.js
@@ -0,0 +1,28 @@
+/* ******************************************** */
+/* * Editable values                          * */
+/* ******************************************** */
+
+// School name, as you would like it displayed
+var school_name = "Luseland School";
+
+var school_short = function(){
+	  if ( typeof school_name_short !== 'undefined') {
+         document.write(school_name_short);
+      }else{
+         document.write(school_name);
+      }
+};
+
+var footer_data = ' <p class="float-right"><a href="#">Back to top</a></p>';
+    footer_data += '<p>Luseland School</p>';
+    footer_data += '<br>';
+    footer_data += '<address>';
+    footer_data += '  Box 399, 701 Prospect Avenue,</br>';
+    footer_data += '  Luseland, SK</br>';
+    footer_data += '  S0L 2A0  </br>';
+    footer_data += '  </br>';
+    footer_data += '  </br>';
+    footer_data += '  Phone: (306)372-4222 Email: luseland@lskysd.ca </br>';
+    footer_data += '</address>';
+
+var general_grad_message = 'Here are graduates from '+school_name+' in the 2019-2020 school year.';
