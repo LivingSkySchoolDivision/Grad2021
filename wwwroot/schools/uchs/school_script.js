29f5d1bcc3e1a3b667332df7817ac51fa2929563
Mike S <mike.strendin@lskysd.ca>
2020-05-27 23:13:13 -0600

    Fixes UCHS school_script.js to add footer and school variables.

--- wwwroot/schools/uchs/school_script.js
+++ wwwroot/schools/uchs/school_script.js
@@ -5,3 +5,25 @@
 // School name, as you would like it displayed
 var school_name = "Unity Composite High School";
 var school_name_short  = "UCHS";
+
+var school_short = function(){
+	  if ( typeof school_name_short !== 'undefined') {
+         document.write(school_name_short);
+      }else{
+         document.write(school_name);
+      }
+};
+
+var footer_data = ' <p class="float-right"><a href="#">Back to top</a></p>';
+    footer_data += '<p>North Battleford Comprehensive High School</p>';
+    footer_data += '<p>"FROM POSSIBILITY TO ACTUALITY"</p>';
+    footer_data += '<br>';
+    footer_data += '<address>';
+    footer_data += '  Box 1000, 3rd Avenue East, </br>';
+    footer_data += '  Unity, SK </br>';
+    footer_data += '  S0K 4L0  </br>';
+    footer_data += '  </br>';
+    footer_data += '  Phone: (306) 228-2657 Email: uchs@lskysd.ca </br>';
+    footer_data += '</address>';
+
+var general_grad_message = 'Here are graduates from '+school_name+' in the 2019-2020 school year.';
