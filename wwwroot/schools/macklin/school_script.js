03c96bdb4e6992adb31ea884414823d7f6c98af9
Mike Strendin <mike.strendin@lskysd.ca>
2021-06-17 09:02:24 -0600

    Removes empty scholarship and awards as requested. Fixes alignment of footer in graduates page for Macklin.

--- wwwroot/schools/macklin/school_script.js
+++ wwwroot/schools/macklin/school_script.js
@@ -14,9 +14,8 @@ var school_short = function(){
 };
 
 var footer_data = ' <p class="float-right"><a href="#">Back to top</a></p>';
-    footer_data += '<p>Macklin School</p>';
-    footer_data += '<br>';
-    footer_data += '<address>';
+    footer_data += '<p style="text-align:left;">Macklin School</p>';
+    footer_data += '<address style="text-align:left;">';
     footer_data += '  Bag #1, 5001 Herald Street, </br>';
     footer_data += '  Macklin, SK</br>';
     footer_data += '  S0L 2C0  </br>';
@@ -25,4 +24,4 @@ var footer_data = ' <p class="float-right"><a href="#">Back to top</a></p>';
     footer_data += '  Phone: (306)753-2375 Email: macklin@lskysd.ca </br>';
     footer_data += '</address>';
 
-var general_school_grad_message = 'Here are graduates from '+school_name+' in the 2019-2020 school year.';
+var general_school_grad_message = 'Here are graduates from '+school_name+' in the 2020-2021 school year.';
